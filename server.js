cf570a4df63368b635b47b7a07920755b8a2fe82
norbert <norberto.a.vega@gmail.com>
2022-07-06 19:38:24 -0300

    Se quita .env del tracking de git. Agrega modo CLUSTER para el servidor, este modo se obtiene de config.js

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 import server from './src/app.js';
-import config from './config.js'
+import config from './config.js';
+import cluster from 'cluster';
+import os from 'os';
 
-server.listen(config.PORT, () => {
-    console.log(`Listening at port ${config.PORT}`);
-});
+function createServer() {
+    server.listen(config.PORT, () => {
+        console.log(`Listening at port ${config.PORT}`);
+    });
+}
+
+const numCpus = os.cpus().length;
+const modo = config.MODO;
+console.log('Modo:', modo);
+
+if (modo === 'FORK')
+    createServer();
+
+else if (modo === 'CLUSTER') {
+    if (cluster.isMaster) {
+        console.log(`Master process ${process.pid}`);
+        for (let i = 0; i < numCpus; i++) {
+            cluster.fork();
+        }
+        cluster.on("listening", (worker, address) => {
+            console.log(`${worker.process.pid} is listening at port ${address.port}`);
+        })
+    }
+    else {
+        createServer();
+        console.log(`worker ${process.pid} started`);
+    }
+}
